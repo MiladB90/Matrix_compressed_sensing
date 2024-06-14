8fcb63276c9bd0565fa74746b469184269c9dee6
Milad Bakhshizadeh <mb4041@columbia.edu>
2024-06-13 19:39:25 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-## sherlock deployment file that requests 1000 cpu cores for 18 hours to run main.py
+## sherlock deployment file that requests 100 cpu cores for 18 hours to run main.py
 #SBATCH --job-name=matrix_completion
 #SBATCH --partition=normal,owners,donoho,hns,stat
 #SBATCH --cpus-per-task=100
