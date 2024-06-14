467f6e196a26dcd9a96eb5b42452454715b2cb69
Milad Bakhshizadeh <mb4041@columbia.edu>
2024-06-13 18:36:14 -0700

    Update run.sh
    mc0014

--- run.sh
+++ run.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-## sherlock deployment file that requests 16 cpu cores for 15 minutes to run main.py
+## sherlock deployment file that requests 1000 cpu cores for 18 hours to run main.py
 #SBATCH --job-name=matrix_completion
 #SBATCH --partition=normal,owners,donoho,hns,stat
-#SBATCH --cpus-per-task=16
-#SBATCH --time=05:00:00
-#SBATCH --error=mc0013.err
-#SBATCH --output=mc0013.out
+#SBATCH --cpus-per-task=1000
+#SBATCH --time=18:00:00
+#SBATCH --error=mc0014.err
+#SBATCH --output=mc0014.out
 
 
 ## Run the python script
