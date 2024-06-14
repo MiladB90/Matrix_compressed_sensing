969b656a5d2d8c5f9c8455b09b29823639bbd12a
Milad Bakhshizadeh <mb4041@columbia.edu>
2024-06-13 22:20:28 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,11 +3,11 @@
 ## sherlock deployment file that requests 100 cpu cores for 18 hours to run main.py
 #SBATCH --job-name=matrix_completion
 #SBATCH --partition=normal,owners,donoho,hns,stat
-#SBATCH --cpus-per-task=100
+#SBATCH --cpus-per-task=1
 #SBATCH --time=18:00:00
 #SBATCH --error=mc0014.err
 #SBATCH --output=mc0014.out
 
 
 ## Run the python script
-time python3 ./experiment.py
+python3 ./experiment.py
