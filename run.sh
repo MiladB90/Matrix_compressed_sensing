1795f8d4577efb02165368f826bb716aff18e092
MiladB90 <mb4041@columbia.edu>
2024-06-21 13:56:53 -0700

    run

--- run.sh
+++ run.sh
@@ -3,8 +3,8 @@
 ## sherlock deployment file that requests 32 cpu cores for 18 hours to run main.py
 #SBATCH --job-name=matrix_compressed_sensing
 #SBATCH --partition=normal,donoho,hns,stat
-#SBATCH --cpus-per-task=64
-#SBATCH --time=36:00:00
+#SBATCH --cpus-per-task=32
+#SBATCH --time=24:00:00
 #SBATCH --error=cs0001.err
 #SBATCH --output=cs0001.out
 
