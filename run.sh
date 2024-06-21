1ace6b7f0ce6a06e93cc756e83b3f0e6bd562f77
Milad Bakhshizadeh <mb4041@columbia.edu>
2024-06-21 13:51:35 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 ## sherlock deployment file that requests 32 cpu cores for 18 hours to run main.py
-#SBATCH --job-name=matrix_completion
+#SBATCH --job-name=matrix_compressed_sensing
 #SBATCH --partition=normal,donoho,hns,stat
 #SBATCH --cpus-per-task=64
 #SBATCH --time=36:00:00
-#SBATCH --error=mc0016.err
-#SBATCH --output=mc0016.out
+#SBATCH --error=cs0001.err
+#SBATCH --output=cs0001.out
 
 
 ## Run the python script
