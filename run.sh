7c0f99c61d5fd298e7ce4b2bf151e46ae3229361
MiladB90 <mb4041@columbia.edu>
2024-06-21 17:40:33 -0700

    push

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 ## sherlock deployment file that requests 32 cpu cores for 18 hours to run main.py
 #SBATCH --job-name=cs01
-#SBATCH --partition=normal,donoho,hns,stat, owners
+#SBATCH --partition=normal,donoho,hns,stat,owners
 #SBATCH --cpus-per-task=8
 #SBATCH --time=01:00:00
 #SBATCH --error=cs0001.err
