4438ec0bd62e722c3ee28ddf23c3b8927d9e22cf
MiladB90 <mb4041@columbia.edu>
2024-07-03 17:18:43 -0700

    6-cpu-1500g-ram

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 ## sherlock deployment file that requests 32 cpu cores for 18 hours to run main.py
 #SBATCH --job-name=cs01
 #SBATCH --partition=normal,donoho,hns,stat,bigmem,dev,
-#SBATCH --cpus-per-task=64
+#SBATCH --cpus-per-task=6
 #SBATCH --mem-per-cpu=256G   # memory per cpu-core
 #SBATCH --time=48:00:00
 #SBATCH --error=cs0001.err
