f696ff6d991abec6c16ab30ef0a20d3e4af8de34
Milad Bakhshizadeh <mb4041@columbia.edu>
2024-07-17 11:29:09 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
 ## sherlock deployment file that requests 32 cpu cores for 18 hours to run main.py
-#SBATCH --job-name=cs01
-#SBATCH --partition=normal,donoho,hns,stat,bigmem
-#SBATCH --cpus-per-task=64
+#SBATCH --job-name=cs02
+#SBATCH --partition=normal,donoho,hns,stat
+#SBATCH --cpus-per-task=128
+
 ##SBATCH --mem-per-cpu=2G  # memory per cpu-core
-#SBATCH --time=24:00:00
-#SBATCH --error=cs0001.err
-#SBATCH --output=cs0001.out
+
+#SBATCH --time=48:00:00
+#SBATCH --error=cs0002.err
+#SBATCH --output=cs0002.out
 
 
 ## Run the python script
